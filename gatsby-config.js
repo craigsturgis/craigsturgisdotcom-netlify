e01d23f49f03302f06c441ce79dda9d0dd47b2d3
Craig Sturgis <craig@csturg.is>
2019-07-17 07:50:55 -0400

    changed title

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Craig Sturgis Dot Com',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Personal Website of Craig Sturgis',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
