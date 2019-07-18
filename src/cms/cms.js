4dae29145122e11d7c09b26cd32f43684bf9e41c
Craig Sturgis <craig@csturg.is>
2019-07-18 12:02:37 -0400

    fix link to renamed blog post preview'

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,7 +3,7 @@ import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
-import BlogPostPreview from './preview-templates/BlogPostPreview'
+import BlogPostPreview from './preview-templates/PostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
