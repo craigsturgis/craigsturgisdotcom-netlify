82875dda60583bf23545791b54144da74bb4b852
Craig Sturgis <craig@csturg.is>
2019-11-27 09:03:49 -0500

    try to fix iphone specific responsiveness on hero image

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -19,7 +19,7 @@ export const IndexPageTemplate = ({
 }) => (
   <div>
     <div
-      className="full-width-image margin-top-0"
+      className="full-width-image-container margin-top-0"
       style={{
         backgroundImage: `url(${
           !!image.childImageSharp ? image.childImageSharp.fluid.src : image
