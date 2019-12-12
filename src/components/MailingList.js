0bc04616dd69e9ef68fc1f5ac5a0f24c1c6ee673
Craig Sturgis <craig@csturg.is>
2019-12-12 09:28:06 -0500

    adding new mailing list component and adding it to posts and index page with a redirect to its typeform

---
+++ src/components/MailingList.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+const MailingList = () => {
+  return (
+    <div>
+      <section className="section">
+        <div className="columns">
+          <div className="column is-4"></div>
+          <div className="column is-4 box has-text-centered">
+            <h2 className="has-text-centered has-text-weight-semibold">
+              Join My Mailing List
+            </h2>
+            <p className="has-text-centered">
+              <Link className="btn" to="/mailing-list">
+                Sign up here
+              </Link>
+            </p>
+            <ul>
+              <li>New Posts in your inbox.</li>
+              <li>Interesting things I want to share.</li>
+              <li>Not too frequent.</li>
+            </ul>
+          </div>
+          <div className="column is-4"></div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default MailingList
